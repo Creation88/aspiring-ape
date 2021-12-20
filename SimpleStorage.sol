83bdfcb6241c70b4ea0c8eaaef0f91b34f4460e2
"Creator" <"creationeightyeight@gmail.com">
2021-12-19 22:31:46 -1000

    Creation

---
+++ SimpleStorage.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.6.0;
+
+contract CreatorStorage {
+
+    uint256 favoriteNumber = 8;
+    bool creatorBool = true;
+    string creatorString = "Creator";
+    int200 creatorInt = -8;
+    address creatorAddress = 209348092348;
+
+
+
+}
