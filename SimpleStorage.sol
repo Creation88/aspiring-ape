9d01fda926fbe51fe4f776ae823199229abc1779
"Creator" <"creationeightyeight@gmail.com">
2021-12-23 19:03:04 -1000

    creation

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -1,13 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+contract TestContract {
+// Some logic
+}
+
 pragma solidity ^0.6.0;
 
 contract CreatorStorage {
 
-    uint256 favoriteNumber = 8;
-    bool creatorBool = true;
-    string creatorString = "Creator";
-    int200 creatorInt = -8;
-    address creatorAddress = 209348092348;
-
-
+    //This will get initiailized to 0
+    uint256 creatorNumber;
+    
+    struct Ppl {
+        uint256 creatorNumber;
+        string name;
+    }
+    Ppl public person = Ppl({creatorNumber: 88, name: "Creator"});
+    
+    function store(uint256 _favoriteNumber) public {
+        creatorNumber = _favoriteNumber;
+    }
+    // view, pure
+    function retrieve() public view returns (uint256) {
+        return creatorNumber;
+    }
 
 }
